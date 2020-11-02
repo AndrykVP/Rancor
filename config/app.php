383529de13e2684c1c245c9a8142fffd9cacb762
Andrés Velázquez <andriclucid@gmail.com>
2020-11-02 04:07:20 -0600

    Better Package Config File

---
+++ config/app.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Auth middleware
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the middleware to use for route authentication
+    | For User models that use an api_token column, use 'auth:api'
+    | For Laravel/Sanctum, use 'auth:sanctum'
+    | Or if you register a custom middleware, specify the guard here
+    |
+    */
+
+    'middleware' => 'auth:api',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Audit User Log Colors
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the colors you want to use for the Rank Change
+    | user logs. This is particularly useful with CSS Frameworks like
+    | Bootstrap, Bulma and Tailwind that use color names in their syntax
+    |
+    */
+
+    'audit' => [
+        'info' => 'blue',
+        'warning' => 'yellow',
+        'alert' => 'red',
+    ]
+];

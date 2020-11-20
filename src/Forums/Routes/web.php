67c20326e6e258e3615e1d1ec8084550af4e5a82
Andrés Velázquez <andriclucid@gmail.com>
2020-11-20 15:44:05 -0600

    Removed Front-end aspect of module

--- src/Forums/Routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-Route::group(['namespace' => 'AndrykVP\Rancor\Forums\Http\Controllers', 'middleware' => ['web']], function(){
-	Route::get('forums','ForumController@index');
-	Route::get('forums/{slug}','ForumController@category');
-	Route::get('forums/board/{slug}','ForumController@board');
-	Route::get('forums/discussion/{id}','ForumController@discussion');
-});

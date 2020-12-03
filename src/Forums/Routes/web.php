f71db6f09b54a0c0e6e58a90e5ac5fe6fd5394b8
Andrés Velázquez <andriclucid@gmail.com>
2020-12-03 17:20:03 -0600

    Better routes

--- src/Forums/Routes/web.php
+++ src/Forums/Routes/web.php
@@ -1,19 +1,21 @@
 <?php 
 
-Route::group(['namespace' => 'AndrykVP\Rancor\Forums\Http\Controllers', 'prefix' => 'forums', 'as' => 'forums.', 'middleware' => ['web']], function(){
-   Route::resources([
-      'boards' => 'BoardController',
-      'categories' => 'CategoryController',
-      // 'discussions' => 'DiscussionController',
-      // 'groups' => 'GroupController',
-      'replies' => 'ReplyController',
-   ]);
+Route::group(['namespace' => 'AndrykVP\Rancor\Forums\Http\Controllers', 'as' => 'forums.', 'middleware' => ['web']], function(){
    
-   Route::get('/','ForumController@index')->name('index');
-   Route::get('unread','ForumController@unread')->name('unread');
-   Route::get('markread','ForumController@markread')->name('markread');
-   Route::get('{category:slug}','ForumController@category')->name('category');
-   Route::get('{category:slug}/{board:slug}','ForumController@board')->name('board');
-   Route::get('{category:slug}/{board:slug}/{discussion}','ForumController@discussion')->name('discussion');
+   Route::prefix('forums')->group(function() {
+      Route::resource('categories','CategoryController')->except('show');
+      Route::resource('boards','BoardController')->except('show');
+      Route::resource('discussions','DiscussionController')->except('show');
+      Route::resource('replies','ReplyController')->except('show','index');
+      Route::resource('groups','GroupController')->except('show');
+      Route::get('{category:slug}','CategoryController@show')->name('categories.show');
+      Route::get('{category:slug}/{board:slug}','BoardController@show')->name('boards.show');
+      Route::get('{category:slug}/{board:slug}/{discussion}','DiscussionController@show')->name('discussions.show');
+      
+      Route::get('/','ForumController@index')->name('index');
+      Route::get('unread','ForumController@unread')->name('unread');
+      Route::get('markread','ForumController@markread')->name('markread');
+   });
+   Route::get('/profile/{user}/replies','ReplyController@index')->name('replies.index');
 });
 

f426db6f6ca2a3a11294348006faf0b4eb6bf4b9
Andrés Velázquez <andriclucid@gmail.com>
2020-12-20 10:03:50 -0600

    Moved API Controller

--- src/Scanner/Routes/api.php
+++ src/Scanner/Routes/api.php
@@ -1,6 +1,6 @@
 <?php 
 
-Route::group(['namespace' => 'AndrykVP\Rancor\Scanner\Http\Controllers', 'prefix' => 'api/scanner', 'middleware' => ['api']], function(){
+Route::group(['namespace' => 'AndrykVP\Rancor\Scanner\Http\Controllers\API', 'prefix' => 'api/scanner', 'middleware' => ['api']], function(){
 	Route::post('search','EntryController@search');
 	Route::apiResources([
 		'entries' => 'EntryController',

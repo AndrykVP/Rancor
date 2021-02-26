31be75557ca106e2554760d5f62131990a48eb27
Andrés Velázquez <andriclucid@gmail.com>
2021-02-26 06:42:39 -0600

    Minor navigation template fix

--- src/Package/Resources/Views/layouts/main.blade.php
+++ src/Package/Resources/Views/layouts/main.blade.php
@@ -58,12 +58,19 @@
                            <a class="nav-link" href="{{ route('admin.index') }}">{{ __('Admin Panel') }}</a>
                            @endif
                         </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="{{ route('forums.index') }}">{{ __('Forums') }}</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="{{ route('news.index') }}">{{ __('Press') }}</a>
+                        </li>
                         <li class="nav-item dropdown">
                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                               {{ Auth::user()->name }} <span class="caret"></span>
                            </a>
 
                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="{{ route('profile.show', ['user' => Auth::user()]) }}">Profile</a>
                               <a class="dropdown-item" href="{{ route('logout') }}"
                               onclick="event.preventDefault();
                               document.getElementById('logout-form').submit();">

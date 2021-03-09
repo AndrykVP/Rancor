4ce7058a82fb7c5cb7e95cc07da0fe3717340c8c
Andrés Velázquez <andriclucid@gmail.com>
2021-03-09 16:11:14 -0600

    Database namespace fix

--- database/seeds/TestSeeder.php
+++ database/seeds/TestSeeder.php
@@ -4,8 +4,11 @@ namespace AndrykVP\Rancor\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use AndrykVP\Rancor\Structure\Models\Faction;
+use AndrykVP\Rancor\Structure\Models\Department;
+use AndrykVP\Rancor\Structure\Models\Rank;
 
-class RancorSeeder extends Seeder
+class TestSeeder extends Seeder
 {
    /**
     * Run the database seeds.
@@ -14,6 +17,8 @@ class RancorSeeder extends Seeder
     */
    public function run()
    {
-      //
+      $factions = Faction::factory()->create(4);
+      $departments = Department::factory()->count(3)->for($factions)->create();
+      $ranks = Rank::factory()->count(12)->for($departments)->create();
    }
 }
--- src/Structure/Models/Faction.php
+++ src/Structure/Models/Faction.php
@@ -4,6 +4,7 @@ namespace AndrykVP\Rancor\Structure\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use AndrykVP\Rancor\Database\Factories\FactionFactory;
 use AndrykVP\Rancor\Structure\Models\Rank;
 use AndrykVP\Rancor\Structure\Models\Department;
 

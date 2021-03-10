0283495e529e383d2cfcd9b176fed7af25a31cd8
Andrés Velázquez <andriclucid@gmail.com>
2021-03-09 19:37:00 -0600

    Simplified TestSeeder

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -18,7 +18,7 @@ use AndrykVP\Rancor\Forums\Models\Discussion;
 use AndrykVP\Rancor\Forums\Models\Group;
 use AndrykVP\Rancor\Forums\Models\Reply;
 use AndrykVP\Rancor\Scanner\Models\Entry;
-use App\User;
+use App\Models\User;
 
 class TestSeeder extends Seeder
 {
@@ -32,11 +32,16 @@ class TestSeeder extends Seeder
       /** 
        * Structure Seeding
        */
-      $factions = Faction::factory()->count(4)->create();
-      $departments = Department::factory()->count(16)->for($factions->random())->create();
-      $ranks = Rank::factory()->count(35)->for($departments->random())->create();
-      $types = Type::factory()->count(4)->create();
-      $awards = Award::factory()->count(24)->for($types->random())->create();
+      $factions = Faction::factory()->count(4)->has(
+         Department::factory()
+                     ->count(3)
+                     ->has(Rank::factory()->count(12))
+                     )->create();
+
+      $types = Type::factory()
+                     ->count(4)
+                     ->has(Award::factory()->count(8))
+                     ->create();
 
       /**
        * User Seeding
@@ -46,31 +51,44 @@ class TestSeeder extends Seeder
       /**
        * News Seeding
        */
-      $tags = Tag::factory()->count(12)->create();
-      $articles = Article::factory()
-                  ->count(35)
-                  ->has($tags->random(3))
-                  ->for($users->random(), 'author')
-                  ->create();
+      $tags = Tag::factory()
+               ->count(12)
+               ->has(Article::factory()
+                     ->count(35)
+                     ->for($users->random(), 'author')
+               )->create();
 
       /**
        * Holocron Seeding
        */
-      $collections = Collection::factory()->count(12)->create();
-      $nodes = Node::factory()
-               ->count(35)
-               ->has($collections->random(3))
-               ->for($users->random(), 'author')
-               ->create();
+      $collections = Collection::factory()
+                     ->count(12)
+                     ->has(Node::factory()
+                           ->count(15)
+                           ->for($users->random(), 'author')
+                     )->create();
 
       /**
        * Forum Seeding
        */
-      $groups = Group::factory()->count(15)->has($users->random(8))->create();
-      $categories = Category::factory()->count(6)->has($groups->random(3))->create();
-      $boards = Board::factory()->count(40)->has($groups->random(2))->has($users->random(), 'moderator')->for($category->random())->create();
-      $discussions = Discussion::factory()->count(250)->for($boards->random())->for($users->random(), 'author')->create();
-      $replies = Reply::factory()->count(5000)->for($discussions->random())->for($users->random(), 'author')->create();
+      $groups = Group::factory()
+                     ->count(15)
+                     ->has(Category::factory()
+                           ->count(6)
+                           ->has(Board::factory()
+                                 ->count(40)
+                                 ->has($users->random(), 'moderator')
+                                 ->has(Discussion::factory()
+                                       ->count(250)
+                                       ->for($users->random(), 'author')
+                                       ->has(Reply::factory()
+                                             ->count(5000)
+                                             ->for($users->random(), 'author')
+                                       )
+                                 )
+                           )
+                     )
+                     ->create();
 
       /**
        * Scanner Seeding

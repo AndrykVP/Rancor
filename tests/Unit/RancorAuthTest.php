2e0d9316319bfea4f7efa4c7744bd77226c64041
Andrés Velázquez <andriclucid@gmail.com>
2021-06-21 20:05:29 -0500

    Add Rancor Auth Test

---
+++ tests/Unit/RancorAuthTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AndrykVP\Rancor\Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use AndrykVP\Rancor\Tests\TestCase;
+use AndrykVP\Rancor\Database\Seeders\RancorSeeder;
+
+class RancorAuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function permissions_and_roles_can_be_created()
+    {
+        $this->seed(RancorSeeder::class);
+        $this->assertDatabaseCount('rancor_permissions', 78);
+        $this->assertDatabaseCount('rancor_roles', 10);
+    }
+}

cbda18f686e594b1b546dfa587e3ef8ecf9f4b79
Andrés Velázquez <andriclucid@gmail.com>
2021-11-08 12:19:56 -0600

    Optimized Online Users feature
    Instead of using View::share in the PackageServiceProvider, which causes errors during testing, the user retrieval has been moved to a View Component instead. It also uses Cache now, to reduce database queries.

---
+++ src/Package/View/Components/OnlineUsers.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AndrykVP\Rancor\Package\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\Support\Facades\Cache;
+use App\Models\User;
+
+class OnlineUsers extends Component
+{
+   /**
+    * Color to use when displaying alert
+    * 
+    * @var Illuminate\Database\Eloquent\Collection
+    */
+   public $users;
+
+   /**
+    * Create a new component instance.
+    *
+    * @return void
+    */
+   public function __construct()
+   {
+      $this->users = Cache::remember('online-users', 60, function () {
+         return User::select('id', 'first_name', 'last_name')
+               ->where('last_seen_at', '>', now()->subMinutes(15))
+               ->get();
+      });
+   }
+
+   /**
+    * Get the view / contents that represent the component.
+    *
+    * @return \Illuminate\Contracts\View\View|string
+    */
+   public function render()
+   {
+      return view('rancor::components.onlineusers');
+   }
+}
